2fe5d9fbb295d2f42c5b830b2bc9cc90ad01dfd1
cmsbuild <>
2008-10-15 10:13:30 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- HcalMonitorModule-CMSSW_3_0_0_pre2'.
    Sprout from master 2008-09-09 16:18:35 UTC Jason St John <sha1-13e6ca3f617e2679b2e43c5867c3ac26779e685c@cern.ch> 'Set 25-event deadcell sigma multiplier to -3.1 (avoids spurious dead cells)'
    Cherrypick from master 2008-10-15 10:13:28 UTC Jason St John <sha1-13e6ca3f617e2679b2e43c5867c3ac26779e685c@cern.ch> 'EventInfo folder using EDFilter DQMEventInfo.  Seems sensible.':
        python/hcal_dqm_source_fileT0_cff.py
        test/expertUseOnly_cfg.py

--- python/hcal_dqm_source_fileT0_cff.py
+++ python/hcal_dqm_source_fileT0_cff.py
@@ -1,17 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
+#from DQMServices.Components.DQMEnvironment_cfi import *
+
+dqmInfoHcal = cms.EDFilter("DQMEventInfo",
+    subSystemFolder = cms.untracked.string('Hcal')
+)
 from DQM.HcalMonitorModule.HcalMonitorModule_cfi import *
-hcalOfflineDQMSource = cms.Sequence(hcalMonitor)
+hcalOfflineDQMSource = cms.Sequence(hcalMonitor + dqmInfoHcal)
 hcalMonitor.DigiOccThresh          = -999999999
 hcalMonitor.PedestalsPerChannel    = False
 hcalMonitor.PedestalsInFC          = True
 hcalMonitor.DataFormatMonitor      = True
-hcalMonitor.DigiMonitor            = False
+hcalMonitor.DigiMonitor            = True
 hcalMonitor.RecHitMonitor          = True
 hcalMonitor.BeamMonitor            = True
 hcalMonitor.TrigPrimMonitor        = True
-hcalMonitor.DeadCellMonitor        = False
-hcalMonitor.HotCellMonitor         = False
+hcalMonitor.DeadCellMonitor        = True
+hcalMonitor.HotCellMonitor         = True
 hcalMonitor.PedestalMonitor        = False
 hcalMonitor.LEDMonitor             = False
 hcalMonitor.MTCCMonitor            = False
